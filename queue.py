42d87c47a5f9915c1466113357031105e7bd8e15
David BELBEZE <david.belbeze@gmail.com>
2018-10-06 10:57:40 +0200

    The queue class is updated to implement FIFO and LIFO logic.

--- queue.py
+++ queue.py
@@ -2,26 +2,43 @@
 
 class Queue:
 
-    def __init__(self, l=[]):
-        """
-        An object which implement the queue logic from a list.
+    FIFO = 0
+    LIFO = 1
 
-        :param l: The base list to use with this queue
-        :type l: list object that is empty by default.
+    def __init__(self, l=[], t=FIFO):
+        """
+            An object which implement the queue logic from a list.
+
+            :param l: The base list to use with this queue. The list is empty by default.
+            :param t: The type of the queue FIFO or LIFO. FIFO by default
+            :type l: list
+            :type t: int
         """
         self.list = l
+        self.__type = t
 
     def push(self, e):
         """
-        Push the param at the end of the queue.
+            Push the param at the end of the queue.
 
-        :param e: The element to insert in the queue.
+            :param e: The element to insert in the queue.
         """
         self.list.append(e)
 
     def pull(self):
-        # TODO: need to implement as FIFO or LIFO
-        return
+        """
+            Pull the element as the type of the queue
+
+            :raise ValueError: If the queue type is not provided.
+            :return: The element to pull as the logic of the queue
+        """
+        if self.__type == self.__class__.FIFO:
+            return self.list.pop(0)
+        elif self.__type == self.__class__LIFO:
+            return self.list.pop(len(self.list) - 1)
+        else:
+            raise ValueError("The type must be set to FIFO or LIFO. See the documentation")
+            return None
 
     def clear(self):
         """
